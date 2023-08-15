16d2f7287dd116b468061e3c356592ad7fbbd46c
itmeOwen <owen23singh@gmail.com>
2023-08-15 20:16:59 +0800

    Added Home Page UI

---
+++ Mobile Game 2 (Prac 1-6) Owen Singh/Assets/Scripts/TileEndBehavior.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles spawning a new tile and destroying this one
+/// upon the player reaching the end
+/// </summary>
+public class TileEndBehavior : MonoBehaviour
+{
+    [Tooltip("How much time to wait before destroying " +
+    "the tile after reaching the end")]
+    public float destroyTime = 1.5f;
+    private void OnTriggerEnter(Collider col)
+    {
+        // First check if we collided with the player
+        if (col.gameObject.GetComponent<PlayerBehavior>())
+        {
+            // If we did, spawn a new tile
+            GameObject.FindObjectOfType<GameController>
+            ().SpawnNextTile();
+            // And destroy this entire tile after a short delay
+            Destroy(transform.parent.gameObject, destroyTime);
+        }
+    }
+}
